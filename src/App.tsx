ecb0c9d8f8f7ed5285c78d5513cb234aa2b617bf
songpengning <pengning.song@versa-ai.com>
2025-03-13 16:48:44 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef, useState } from 'react'
+import './assets/css/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>SlightHeat</div>
+    </>
+  )
+}
+
+export default App
